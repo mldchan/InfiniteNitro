dc76d7b2ee5930ffd8726987186e57ec01a41c36
Akatsuki <mldkyt@proton.me>
2024-05-19 18:02:57 +0200

    Initial commit

---
+++ InfiniteNitro/InfiniteNitro.cs
@@ -0,0 +1,31 @@
+﻿using HutongGames.PlayMaker;
+using JetBrains.Annotations;
+using MSCLoader;
+using UnityEngine;
+
+namespace InfiniteNitro
+{
+    [UsedImplicitly]
+    public class InfiniteNitro : Mod
+    {
+        public override string ID => "InfiniteNitro";
+        public override string Version => "1.0";
+        public override string Author => "アカツキ";
+
+        public override void ModSetup()
+        {
+            base.ModSetup();
+            
+            SetupFunction(Setup.Update, On_Update);
+        }
+
+        private void On_Update()
+        {
+            var n2o = GameObject.Find("n2o bottle(Clone)/Dynamics");
+            if (n2o != null && n2o.activeSelf)
+            {
+                n2o.GetPlayMaker("Pressure").GetVariable<FsmFloat>("Fluid").Value = 5;
+            }
+        }
+    }
+}
